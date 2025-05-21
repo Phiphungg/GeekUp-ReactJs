373d8a6ce3d63060fbd93d9d48b8120d21310b96
Tran Phi Phung <phiphung0102@gmail.com>
2025-05-21 16:58:07 +0700

    fix file vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/GeekUp-Reactjs",
+  // eslint-disable-next-line no-undef
+  base: process.env.VITE_BASE_PATH || "/GeekUp-ReactJs",
   server: {
     historyApiFallback: true, // quan trọng cho route nội bộ khi mở tab mới
   },
